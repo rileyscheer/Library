73ab385cfe7d220c333cd92c6171aaf2ce201bb0
rileyscheer <68623637+rileyscheer@users.noreply.github.com>
2022-03-03 02:12:19 -0600

    cards now function properly

--- script.js
+++ script.js
@@ -21,12 +21,22 @@ function addBookToLibrary() {
     displayBooks()
 }
 
+function clearInputs() {
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+    document.querySelector('.pages').value = '';
+    radio = document.querySelector('.yesno');
+    radio.checked = false;
+}
+
+
 add.addEventListener('click', addBookToLibrary)
 
 function displayBooks() {
+    // Creating and styling cards
     const newCard = document.createElement('div')
     newCard.style.background = 'white';
-    newCard.style.height = '200px';
+    newCard.style.height = '310px';
     newCard.style.width = '400px'
     newCard.style.borderRadius = '25px';
     newCard.style.margin = '30px';
@@ -36,41 +46,105 @@ function displayBooks() {
     newCard.style.flexDirection = 'column';
     newCard.style.fontSize = '24px';
     newCard.style.letterSpacing = '1px';
-    newCard.style.boxShadow = '0px 0px 20px'
-    newCard.style.fontWeight = '500'
+    newCard.style.boxShadow = '0px 0px 20px';
+    newCard.style.fontWeight = '500';
 
+
+    // Adding text to card
     const titleContent = document.createTextNode(`Title: ${library[j].title}`)
     const line = document.createElement('br')
+    const line2 = document.createElement('br')
     newCard.appendChild(titleContent)
     newCard.appendChild(line)
-
-    const authorContent = document.createTextNode(`Author: ${library[j].author}`)
-    const line2 = document.createElement('br')
-    newCard.appendChild(authorContent)
     newCard.appendChild(line2)
 
-    const pageNumber = document.createTextNode(`Pages: ${library[j].pages}`)
+    const authorContent = document.createTextNode(`Author: ${library[j].author}`)
     const line3 = document.createElement('br')
-    newCard.appendChild(pageNumber)
-    newCard.appendChild(line3)
-
-    const haveRead = document.createTextNode(`Have read? ${library[j].read}`)
     const line4 = document.createElement('br')
-    newCard.appendChild(haveRead)
+    newCard.appendChild(authorContent)
+    newCard.appendChild(line3)
     newCard.appendChild(line4)
 
-    j += 1;
+    const pageNumber = document.createTextNode(`Pages: ${library[j].pages}`)
+    const line5 = document.createElement('br')
+    const line6 = document.createElement('br')
+    newCard.appendChild(pageNumber)
+    newCard.appendChild(line5)
+    newCard.appendChild(line6)
+
+
+    // Creating read or unread button
+    const newButton = document.createElement('button')
+    newButton.style.width = '200px';
+    newButton.style.height = '50px';
+    newButton.style.borderRadius = '25px';
+    newButton.style.border = 'solid black 2px';
+    newButton.style.fontWeight = '500';
+    newButton.style.color = 'black';
+
+    radio = document.querySelector('.yesno:checked').value;
+    if (radio === 'Yes'){
+        newButton.style.background = '#47D65A';
+        newButton.textContent = 'Read';
+        newButton.onmouseover = function() {
+            newButton.style.background = 'rgb(68, 0, 0)';
+            newButton.style.color = 'white';
+        }
+        newButton.onmouseout = function() {
+            newButton.style.background = '#47D65A';
+            newButton.style.color = 'black';
+        }
+        newButton.addEventListener('click', function() {
+            newButton.style.background = 'rgb(68, 0, 0)'
+            newButton.style.color = 'white';
+        })
+        clearInputs()
+    }   else if(radio === 'No') {
+        newButton.style.background = 'rgb(68, 0, 0)';
+        newButton.style.color = 'white';
+        newButton.textContent = 'Unread';
+        newButton.onmouseover = function() {
+            newButton.style.background = '#47D65A';
+            newButton.style.color = 'black';
+        }
+        newButton.onmouseout = function() {
+            newButton.style.background = 'rgb(68, 0, 0)';
+            newButton.style.color = 'white';
+        }
+        newButton.addEventListener('click', function() {
+            newButton.style.background = '#47D65A';
+            newButton.style.color = 'black';
+        })
+        clearInputs()
+    }
+    newCard.appendChild(newButton)
+    const line7 = document.createElement('br')
+    newCard.appendChild(line7)
+
+
+    // Creating delete button
+    const deleteBtn = document.createElement('button')
+    deleteBtn.style.width = '90px';
+    deleteBtn.style.height = '35px';
+    deleteBtn.style.background = 'black';
+    deleteBtn.style.color = 'white';
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.style.borderRadius = '15px';
+    deleteBtn.style.border = 'solid 1px black';
+    deleteBtn.style.fontSize = '16px';
+
+    newCard.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', function(){
+        newCard.remove()
+    })
+
+    j += 1;  //Used to iterate through library
 
     for (i in library) {
         makeCard.appendChild(newCard)
-        clearInputs();
+        clearInputs()
     }
-}
-
-function clearInputs() {
-    document.querySelector('.title').value = '';
-    document.querySelector('.author').value = '';
-    document.querySelector('.pages').value = '';
 }
 
 
